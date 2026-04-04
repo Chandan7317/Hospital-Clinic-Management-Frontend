515d073839812a02765eac85f2c24430e3bbb79a
Chandan Kumar Rajbhar <crajbhar8736@gmail.com>
2026-04-04 23:50:30 +0530

    add icon in card section

--- src/Pages/homepage/HomePage.jsx
+++ src/Pages/homepage/HomePage.jsx
@@ -40,21 +40,30 @@ const HomePage = () => {
       </div>
 
       {/* Card */}
-
       <div className="grid grid-cols-1 gap-6 sm:grid-cols-2 md:grid-cols-3 w-[90%] mx-auto my-5">
-        {Card.map((ele ) => {
-          const { id, image, heading, content } = ele;
+        {Card.map((ele) => {
+          // console.log(ele);
+          const { id, heading, content, icon: Icon, bgc } = ele; // icon as component
 
           return (
-            <div key={id} className={`card w-full shadow-sm ${ele.bgc}`}>
-              <div className="card-body">
-                <h2 className="card-title">{heading}</h2>
-                <p className="font-serif">{content}</p>
+            <div
+              key={id}
+              className={`card w-full shadow-sm  text-black ${bgc}`}
+            >
+              <div className="card-body flex flex-col items-start gap-3">
+                <figure className="w-full flex justify-center mb-3">
+                  {" "}
+                  {Icon && <Icon size={35} className="text-blue-600" />}
+                </figure>
+                <h2 className="card-title text-lg font-bold">{heading}</h2>
+                <p className="font-serif ">{content}</p>
               </div>
             </div>
           );
         })}
       </div>
+
+    
     </HomeLayout>
   );
 };
--- src/utils/Constants/homepages/Card.js
+++ src/utils/Constants/homepages/Card.js
@@ -1,42 +1,44 @@
+import { FaUserDoctor } from "react-icons/fa6";
 export const Card = [{
     id: 1,
-    image: "☻",
+    icon: FaUserDoctor, // component reference
     heading: "You're juggling multiple tools to manage patient records, billing, and consultations",
     content: "Switch to one integrated platform that centralizes patient care, administration, and communication—built for hospitals, clinics, and tele health providers.",
     bgc: "BgColor_1"
 }, {
     id: 2,
-    image: "☻",
+    icon: FaUserDoctor, // component reference
     heading: "Your patients struggle to book appointments or get updates across platforms",
     content: "Provide seamless access via web, mobile app, WhatsApp, and SMS, so your patients can easily book appointments, receive reminders, or get support anytime, anywhere.",
     bgc: "BgColor_2"
 },
 {
     id: 3,
-    image: "☻",
+    icon: FaUserDoctor, // component reference
     heading: "You're unsure if your healthcare data is truly protected",
     content: "Rely on enterprise-grade security with role-based access, audit trails, and HIPAA-compliant architecture to safeguard patient data.",
     bgc: "BgColor_3"
 },
 {
     id: 4,
-    image: "☻",
+    icon: FaUserDoctor, // component reference
     heading: "New software setups feel slow, complex, and hard to adapt",
     content: "Get started in days, not weeks—with flexible modules tailored to your workflows and zero-code customization options.",
     bgc: "BgColor_4"
 },
 {
     id: 5,
-    image: "☻",
+    icon: FaUserDoctor, // component reference
     heading: "Hospital Management System (HMS)",
     content: "Streamline clinical and administrative tasks with effortless OPD/IPD, billing, inventory, and analytics.",
     bgc: "BgColor_5"
 },
 {
     id: 6,
-    image: "☻",
+    icon: FaUserDoctor,// component reference
     heading: "Queue Management",
     content: "mprove patient flow with token-based queuing, live display screens, and SMS notifications.",
     bgc: "BgColor_6"
 }
 ]
+
